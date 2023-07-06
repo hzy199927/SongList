9ac310bbdb152f335d94d0034c394004d93d3c2b
于桑 <2732527367@qq.com>
2023-07-06 15:20:08 +0800

    添加了music-app

--- music-app/src/router/index.js
+++ music-app/src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import {info} from '../api/users'
-import { useUsersStore } from '../stores/users'
+// import {info} from '../api/users'
+// import { useUsersStore } from '../stores/users'
 
 const Index = ( ) => import('@/views/Index.vue')
 const Find = ( ) => import('@/views/Find.vue')
@@ -16,64 +16,48 @@ const router =createRouter ({
       path:'/',
       name:'index',
       component:Index,
-      redirect:'/home',
-      meta:{
-        auth:true
-      }
     },
     {
       path:'/like',
       name:'like',
       component:Like,
-      meta: {
-        auth:false
-      }
     },
     {
       path:'/login',
       name:'login',
       component:Login,
-      meta: {
-        auth:false
-      }
     },
     {
       path:'/find',
       name: 'find',
       component:Find,
-      meta: {
-        auth:false
-      }
     },
     {
       path:'/video',
       name: 'video',
       component:Video,
-      meta: {
-        auth:false
-      }
     },
   ]
 })
 
-router.beforeEach((to, from, next) => {
+// router.beforeEach((to, from, next) => {
 
-  if(to.meta.auth) {  
-    info().then((res) => {
-      if(res.data.errcode === 0) {
-        const usersStore = useUsersStore()
-        usersStore.updateUsername(res.data.username)
-        next()
-      }
-      else {
-        next('/login')
-      }
-    })
+//   if(to.meta.auth) {  
+//     info().then((res) => {
+//       if(res.data.errcode === 0) {
+//         const usersStore = useUsersStore()
+//         usersStore.updateUsername(res.data.username)
+//         next()
+//       }
+//       else {
+//         next('/login')
+//       }
+//     })
 
-  }
-  else {
-    next()
-  }
+//   }
+//   else {
+//     next()
+//   }
 
-})
+// })
 export default router
